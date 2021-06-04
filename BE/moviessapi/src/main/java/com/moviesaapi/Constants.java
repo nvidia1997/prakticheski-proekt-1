94de03d724c2a50681052f40388bac196891315b
nvidia1997 <nvidia1997@gmail.com>
2021-06-04 11:05:37 +0300

    added initial web project & added cors config

--- BE/moviessapi/src/main/java/com/moviesaapi/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.moviesaapi;
-
-public final class Constants {
-    public static final String ORIGINS = "http://localhost:8080";
-    public static final String EXPOSED_HEADERS = "Access-Control-Allow-Origin=*";
-}
